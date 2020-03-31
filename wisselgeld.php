6381c71acf92e2cbc87bce0a8b254d0500570282
Sam Ahmed <2102926@talnet.nl>
2020-03-31 14:21:34 +0200

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,17 +1,24 @@
 <?php
-$geld = array('50', '20', '10', '5', '2', '1');
-$aantal = $argv[1];
+$geld      = array(
+    '50',
+    '20',
+    '10',
+    '5',
+    '2',
+    '1'
+);
+$aantal    = $argv[1];
 $restanten = $aantal;
 foreach ($geld as $value) {
-  if ($argv[1] == $value) {
-    continue;
-  }
+    if ($argv[1] == $value) {
+        continue;
+    }
     $totaal = floor($restanten / $value);
     if ($totaal > 0) {
-      echo "U krijgt $totaal x €$value " . PHP_EOL;
-      $aantal = $restanten;
-      $restanten = floor($restanten % $value);
-  }
+        echo "U krijgt $totaal x €$value " . PHP_EOL;
+        $aantal    = $restanten;
+        $restanten = floor($restanten % $value);
+    }
 }
 
- ?>
+?>
